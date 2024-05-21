336bbf1372601e31a4ad46701e3213037cb088f1
tech-aditea <164619001+tech-aditea@users.noreply.github.com>
2024-05-21 21:50:02 +0530

    skp.py

--- skp.py
+++ skp.py
@@ -1,11 +1,18 @@
 from skpy import Skype
 
-slogin = Skype("ditea4u@gmail.com","automate4m@m")
-contact = slogin.contacts 
+slogin = Skype("your_skype_email/username","your_skype_password")
+
+#FOR SENDING AUTOMATED MSSG TO GROUP
+#group = slogin.chats.create(["skype_username_or_email_of_recipient_1","skype_username_or_email_of_recipient_2","skype_username_or_email_of_recipient_3"])
+
+#FOR SENDING AUTOMATED MSSG TO PARTICULAR PERSON  
+#contact = slogin.contacts["skype_username_or_email_of_recipient"] 
 #contact.chat.sendMsg(" Welcome ")
 
-for i in contact :
-    print(i)
+#FOR VIEWING ENTIRE CONTACT LIST
+#contact = slogin.contacts 
+#for i in contact :
+#   print(i)
 
 
 
